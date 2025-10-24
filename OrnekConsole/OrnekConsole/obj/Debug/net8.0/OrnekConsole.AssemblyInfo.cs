5e63f48c5da09c66839375934d2289dd0ad56b2d
alpceyhan86 <alpceyhan86@gmail.com>
2025-10-24 10:13:12 +0300

    ikinci

--- OrnekConsole/OrnekConsole/obj/Debug/net8.0/OrnekConsole.AssemblyInfo.cs
+++ OrnekConsole/OrnekConsole/obj/Debug/net8.0/OrnekConsole.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrnekConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf0d98ca02936affdd9b07da1ca5f0e1acdfcc02")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrnekConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrnekConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
