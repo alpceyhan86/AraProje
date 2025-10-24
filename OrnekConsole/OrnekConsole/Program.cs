0a55ae51ed5c5a2a839aa1f975144fda878d70c8
alpceyhan86 <alpceyhan86@gmail.com>
2025-10-24 11:29:51 +0300

    Üçüncü

--- OrnekConsole/OrnekConsole/Program.cs
+++ OrnekConsole/OrnekConsole/Program.cs
@@ -5,7 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Değişilik");
+            Console.WriteLine("Değişiklik");
+            Console.WriteLine("Değişiklik2");
         }
     }
 }
